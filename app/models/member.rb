4bacd42fc750dbb149d7292f2260bc0c92895aaa
Chris Westra <cwestra@gmail.com>
2013-10-25 17:37:48 -0400

    Reorganize Member.

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,24 +1,20 @@
 class Member < ActiveRecord::Base
-  attr_accessible :address, :city, :date_joined, :first_name, :height,
-    :last_name, :photo_path, :program_name, :publish_data,
-    :state, :status_id, :voice_part_id, :zip, :email_addresses_attributes,
-    :phone_numbers_attributes, :photo, :no_chairs
-
-
-  #items for photo upload via paperclip, cropping via jcrop
-  attr_accessor :crop_x, :crop_y, :crop_w, :crop_h
-  attr_accessible :crop_x, :crop_y, :crop_w, :crop_h
+  has_many :chair_schedules, :dependent => :destroy
+  has_many :phone_numbers, :dependent => :destroy
+  has_one :folder, :dependent => :destroy
+  belongs_to :voice_part
+  has_many :email_addresses, :dependent => :destroy
+  belongs_to :member_status, foreign_key: :status_id
 
   scope :active, -> { where(status_id: 1) }
 
-  def cropping?
-    !crop_x.blank? && !crop_y.blank? && !crop_w.blank? && !crop_h.blank?
-  end
+  attrs = %i(address city state zip date_joined
+             first_name last_name program_name
+             height photo_path photo publish_data
+             status_id voice_part_id email_addresses_attributes
+             phone_numbers_attributes no_chairs)
 
-  def photo_geometry(style = :original)
-    @geometry ||= {}
-    @geometry[style] ||= Paperclip::Geometry.from_file(photo.path(style))
-  end
+  attr_accessible *attrs
 
   def check_program_name
     if program_name.present?
@@ -45,6 +41,14 @@ class Member < ActiveRecord::Base
     e.address if e
   end
 
+
+
+  validates :first_name, :last_name, :status_id, :voice_part_id, :presence => true
+  accepts_nested_attributes_for :phone_numbers, :reject_if => lambda { |a| a[:number].blank? }, allow_destroy: true
+  accepts_nested_attributes_for :email_addresses, :reject_if => lambda { |a| a[:address].blank? }, allow_destroy: true
+
+  #items for photo upload via paperclip, cropping via jcrop
+
   has_attached_file :photo, :styles => {:thumb => ["95x120#", :jpg], :large => ["500x500>", :jpg]},
     :processors => [:cropper],
 
@@ -54,20 +58,20 @@ class Member < ActiveRecord::Base
 
   validates_attachment_size :photo, :less_than => 2.megabytes
 
+  attr_accessible :crop_x, :crop_y, :crop_w, :crop_h
+
+  def reprocess_photo
+    photo.reprocess!
+  end
+
+  def cropping?
+    !crop_x.blank? && !crop_y.blank? && !crop_w.blank? && !crop_h.blank?
+  end
+
+  def photo_geometry(style = :original)
+    @geometry ||= {}
+    @geometry[style] ||= Paperclip::Geometry.from_file(photo.path(style))
+  end
 
 
-
-  validates :first_name, :last_name, :status_id, :voice_part_id, :presence => true
-  has_many :chair_schedules, :dependent => :destroy
-  has_many :phone_numbers, :dependent => :destroy
-  has_one :folder, :dependent => :destroy
-  belongs_to :voice_part
-  has_many :email_addresses, :dependent => :destroy
-  belongs_to :member_status, foreign_key: :status_id
-  accepts_nested_attributes_for :phone_numbers, :reject_if => lambda { |a| a[:number].blank? }, allow_destroy: true
-  accepts_nested_attributes_for :email_addresses, :reject_if => lambda { |a| a[:address].blank? }, allow_destroy: true
-
-    def reprocess_photo
-      photo.reprocess!
-    end
 end
