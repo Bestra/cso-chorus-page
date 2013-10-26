a179320463d3db020bf9c6e74edda7ed9833012e
Chris Westra <cwestra@gmail.com>
2013-10-26 16:53:09 -0400

    Use best in place for member status.

--- app/models/member_status.rb
+++ app/models/member_status.rb
@@ -1,4 +1,10 @@
 class MemberStatus < ActiveRecord::Base
   attr_accessible :description
   has_many :members
+
+  def self.select_options
+    MemberStatus.all.map do |s|
+      [s.id, s.description]
+    end
+  end
 end
